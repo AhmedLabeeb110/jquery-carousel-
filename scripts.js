fe807b6b69d3eda57ea5205e6dc26b1b9318f338
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-02-01 15:18:17 +0600

    scripts path fixed

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
         if(nextImg.length){
             currentImg.removeClass('active').css('z-index', -10);
-            nextImg.addClass('active').css('z-index', 10);
+            nextImg.addClass('active animate__animated animate__slideInRight').css('z-index', 10);
         }
     })
     $('.prev').on('click', function(){
@@ -14,7 +14,7 @@ $(document).ready(function(){
     
         if(prevImg.length){
             currentImg.removeClass('active').css('z-index', -10);
-            prevImg.addClass('active').css('z-index', 10);
+            prevImg.addClass('active animate__animated animate__slideInLeft').css('z-index', 10);
         }
     })
 })
